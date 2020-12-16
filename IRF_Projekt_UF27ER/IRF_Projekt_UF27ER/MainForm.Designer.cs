bb0fe15bedc1499ae38ad6dba6beceaf669dba37
EOM563 <kissgy97@gmail.com>
2020-12-16 20:41:03 +0100

    last edit

--- IRF_Projekt_UF27ER/IRF_Projekt_UF27ER/MainForm.Designer.cs
+++ IRF_Projekt_UF27ER/IRF_Projekt_UF27ER/MainForm.Designer.cs
@@ -31,6 +31,8 @@
             this.buttonFormNelkul = new System.Windows.Forms.Button();
             this.buttonFormazott = new System.Windows.Forms.Button();
             this.buttonExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonFormNelkul
@@ -69,12 +71,34 @@
             this.buttonExit.UseVisualStyleBackColor = true;
             this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(886, 208);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 65);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "A gombokra kattintva válassza \r\nki a kiírni kívánt elemeket.\r\n\r\nKategória gombra " +
+    "kattintva az \r\nösszes odatartozó elemet kijelöli.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(872, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Használat:";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1062, 461);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonExit);
             this.Controls.Add(this.buttonFormazott);
             this.Controls.Add(this.buttonFormNelkul);
@@ -84,6 +108,7 @@
             this.Text = "Árukészlet listázás";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -92,6 +117,8 @@
         private System.Windows.Forms.Button buttonFormNelkul;
         private System.Windows.Forms.Button buttonFormazott;
         private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
